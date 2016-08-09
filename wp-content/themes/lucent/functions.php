847f2ff305f42761a8aaa83dbb5d2831d4f7341e
Robert M. Parker <rparker@yamiko.ninja>
2016-08-09 01:55:26 -0700

    icons row

--- wp-content/themes/lucent/functions.php
+++ wp-content/themes/lucent/functions.php
@@ -1,3 +1,9 @@
 <?php
 require_once __DIR__.'/app/bootstrap.php';
 require_once __DIR__.'/src/functions.php';
+
+
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
--- wp-content/themes/lucent/index.php
+++ wp-content/themes/lucent/index.php
@@ -1,5 +1,13 @@
 <?php
 require_once __DIR__.'/app/bootstrap.php';
-
 $twig = $container->get("twig.environment");
-echo $twig->render('basic.html.twig');
+$data = [];
+$rows = [];
+while(have_rows('content')) {
+    the_row();
+    $rows[] = $twig->render('panels/'.get_row_layout().'.html.twig', $data);
+}
+$data['rows'] = $rows;
+
+// render
+echo $twig->render('basic.html.twig', $data);
