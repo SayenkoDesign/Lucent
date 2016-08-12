0447efc767759912697c8311288c0b0bdb56017c
Robert M. Parker <rparker@yamiko.ninja>
2016-08-12 14:59:01 -0700

    call to action

--- wp-content/themes/lucent/functions.php
+++ wp-content/themes/lucent/functions.php
@@ -7,3 +7,14 @@ add_filter('upload_mimes', function ($mimes) {
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 });
+
+
+add_action('init', function() {
+    if (function_exists('acf_add_options_page')) {
+        acf_add_options_page([
+            'page_title' => 'Theme Options',
+            'capability' => 'edit_theme_options',
+            'icon_url' => 'dashicons-sayenko',
+        ]);
+    }
+});
