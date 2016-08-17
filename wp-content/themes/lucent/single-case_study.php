c6e498835ec3af2bce083b64879a6b3911ca0e5a
Robert M. Parker <rparker@yamiko.ninja>
2016-08-17 15:30:35 -0700

    single cases

---
+++ wp-content/themes/lucent/single-case_study.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/app/bootstrap.php';
+$twig = $container->get("twig.environment");
+$data = [];
+$rows = [];
+while(have_rows('content')) {
+    the_row();
+    $rows[] = $twig->render('panels/'.get_row_layout().'.html.twig', $data);
+}
+$data['rows'] = $rows;
+
+ob_start();
+previous_post_link('%link', '&lt; Previous');
+$data['previous'] = ob_get_clean();
+ob_start();
+next_post_link('%link', 'Previous &gt;');
+$data['next'] = ob_get_clean();
+
+// render
+echo $twig->render('single-case-study.html.twig', $data);
