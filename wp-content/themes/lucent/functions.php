86cb6a3e0b12a7bf0b44621e06b3ab97b9171ff9
Robert M. Parker <rparker@yamiko.ninja>
2016-08-18 15:51:51 -0700

    google analytics

--- wp-content/themes/lucent/functions.php
+++ wp-content/themes/lucent/functions.php
@@ -2,12 +2,10 @@
 require_once __DIR__.'/app/bootstrap.php';
 require_once __DIR__.'/src/functions.php';
 
-
 add_filter('upload_mimes', function ($mimes) {
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 });
-
 
 add_action('init', function() {
     if (function_exists('acf_add_options_page')) {
@@ -18,3 +16,21 @@ add_action('init', function() {
         ]);
     }
 });
+
+// google analytics
+// only add if ACF is defined and value is found
+if(function_exists('acf_add_options_page') && $googleID = get_field('google_analytics_id', 'option')) {
+    add_action('wp_head', function () use ($googleID) {
+        echo <<<HTML
+            <script>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                
+                ga('create', '$googleID', 'auto');
+                ga('send', 'pageview');
+            </script>
+HTML;
+    });
+};
