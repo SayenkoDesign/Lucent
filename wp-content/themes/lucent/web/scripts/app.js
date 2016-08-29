4342a1c63bfaaac16fba2cd296febbde24cc1e55
Robert M. Parker <rparker@yamiko.ninja>
2016-08-29 12:59:29 -0700

    case study slider tweaks

--- wp-content/themes/lucent/web/scripts/app.js
+++ wp-content/themes/lucent/web/scripts/app.js
@@ -1,17 +1,20 @@
 jQuery(function() {
     jQuery(document).foundation();
-    jQuery('.fancybox').fancybox();
+    jQuery('.fancybox, li.contact > a').fancybox();
     jQuery('.slick').slick({
         arrows: true,
-        slidesToShow: 4,
+        centerMode: true,
+        slidesToShow: 3,
         responsive: [{
             breakpoint: 700,
             settings: {
-                slidesToShow: 1
+                slidesToShow: 1,
+                centerMode: true
             },
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2
+                slidesToShow: 2,
+                centerMode: true
             }
         }]
     });
